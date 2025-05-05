1863a4d94f2020855490df90f6187373aacd81f0
lisandre-begon <lisandre.begon1@gmail.com>
2025-05-06 11:56:12 +1200

    Add requirements.txt

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from cleanTxt import process_text_file
+from dotenv import load_dotenv
 import os
 
+load_dotenv()
 
-folder = "textFiles"
+folder = os.getenv("TEXT_FILES")
 
 for file in os.listdir(folder):
     if file.endswith(".txt"):
         file_path = os.path.join(folder, file)
         print(f"Processing {file_path}...")
-        result = process_text_file(file_path)
+        result = process_text_file(file_path, output_folder=os.getenv("TEXT_FILES_CLEANED"))
         print(f"Saved to: {result['output_path']}")
         print(f"Format detected: {result['mode']}\n")
